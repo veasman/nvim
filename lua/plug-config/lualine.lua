ca58a66e6419761315fde486df0b1ebde9b75fc4
veasman <charlton.moren@protonmail.com>
2022-04-26 03:23:32 -0500

    update lualine config

--- lua/plug-config/lualine.lua
+++ lua/plug-config/lualine.lua
@@ -3,21 +3,28 @@ if not status_ok then
 	return
 end
 
--- Color table for highlights
--- stylua: ignore
--- TODO: Pull colors from colorscheme
-local colors = {
-  bg       = '#202328',
-  fg       = '#bbc2cf',
-  yellow   = '#ECBE7B',
-  cyan     = '#008080',
-  darkblue = '#081633',
-  green    = '#98be65',
-  orange   = '#FF8800',
-  violet   = '#a9a1e1',
-  magenta  = '#c678dd',
-  blue     = '#51afef',
-  red      = '#ec5f67',
+local colors = require("gruvbox-baby.colors").config()
+
+local mode_color = {
+  n = colors.blue_gray,
+  i = colors.soft_green,
+  v = colors.orange,
+  V = colors.orange,
+  c = colors.magenta,
+  no = colors.red,
+  s = colors.orange,
+  S = colors.orange,
+  [''] = colors.orange,
+  ic = colors.yellow,
+  R = colors.red,
+  Rv = colors.violet,
+  cv = colors.red,
+  ce = colors.red,
+  r = colors.cyan,
+  rm = colors.cyan,
+  ['r?'] = colors.cyan,
+  ['!'] = colors.red,
+  t = colors.red,
 }
 
 local conditions = {
@@ -34,137 +41,102 @@ local conditions = {
   end,
 }
 
--- Config
 local config = {
   options = {
-    -- Disable sections and component separators
-    component_separators = '',
-    section_separators = '',
+    section_separators = { left = '', right = '' },
+    component_separators = { left = '|', right = '|' },
     theme = {
-      -- We are going to use lualine_c an lualine_x as left and
-      -- right section. Both are highlighted by c theme .  So we
-      -- are just setting default looks o statusline
-      normal = { c = { fg = colors.fg, bg = colors.bg } },
-      inactive = { c = { fg = colors.fg, bg = colors.bg } },
+      normal = {
+        a = { fg = colors.foreground, bg = colors.background_dark },
+        b = { fg = colors.foreground, bg = colors.background_dark },
+        c = { fg = colors.foreground, bg = colors.background_dark },
+        x = { fg = colors.foreground, bg = colors.background_dark },
+        y = { fg = colors.foreground, bg = colors.background_dark },
+        z = { fg = colors.foreground, bg = colors.background_dark }
+      },
+      inactive = {
+        a = { fg = colors.foreground, bg = colors.background_dark },
+        b = { fg = colors.foreground, bg = colors.background_dark },
+        c = { fg = colors.foreground, bg = colors.background_dark },
+        x = { fg = colors.foreground, bg = colors.background_dark },
+        y = { fg = colors.foreground, bg = colors.background_dark },
+        z = { fg = colors.foreground, bg = colors.background_dark }
+      },
     },
   },
   sections = {
-    -- these are to remove the defaults
-    lualine_a = {},
-    lualine_b = {},
-    lualine_y = {},
-    lualine_z = {},
-    -- These will be filled later
-    lualine_c = {},
-    lualine_x = {},
-  },
-  inactive_sections = {
-    -- these are to remove the defaults
-    lualine_a = {},
-    lualine_b = {},
-    lualine_y = {},
-    lualine_z = {},
-    lualine_c = {},
-    lualine_x = {},
-  },
-}
-
--- Inserts a component in lualine_c at left section
-local function ins_left(component)
-  table.insert(config.sections.lualine_c, component)
-end
-
--- Inserts a component in lualine_x ot right section
-local function ins_right(component)
-  table.insert(config.sections.lualine_x, component)
-end
-
-ins_left {
-  -- mode component
-  "mode",
-  function(str)
-    return str
-  end,
-  color = function()
-    -- auto change color according to neovims mode
-    local mode_color = {
-      n = colors.blue,
-      i = colors.green,
-      v = colors.orange,
-      V = colors.orange,
-      c = colors.magenta,
-      no = colors.red,
-      s = colors.orange,
-      S = colors.orange,
-      [''] = colors.orange,
-      ic = colors.yellow,
-      R = colors.violet,
-      Rv = colors.violet,
-      cv = colors.red,
-      ce = colors.red,
-      r = colors.cyan,
-      rm = colors.cyan,
-      ['r?'] = colors.cyan,
-      ['!'] = colors.red,
-      t = colors.red,
-    }
-    return { bg = mode_color[vim.fn.mode()], fg = colors.bg }
-  end,
-  padding = { left = 1, right = 1 },
-}
-
-ins_left {
-  'filename',
-  cond = conditions.buffer_not_empty,
-  color = { fg = colors.magenta, gui = 'bold' },
-}
-
-ins_left {
-  'diff',
-  symbols = { added = ' ', modified = ' ', removed = ' ' },
-  diff_color = {
-    added = { fg = colors.green },
-    modified = { fg = colors.orange },
-    removed = { fg = colors.red },
-  },
-  cond = conditions.hide_in_width,
-}
-
-ins_left {
-  'branch',
-  icon = '',
-  color = { fg = colors.blue, gui = 'bold' },
-}
-
-ins_right {
-  "filetype",
-  icons_enabled = true,
-  icon = nil,
-  color = { fg = colors.green, gui = 'bold' },
-}
-
-ins_right {
-  'diagnostics',
-  sources = { 'nvim_diagnostic' },
-  symbols = { error = ' ', warn = ' ', info = ' ' },
-  diagnostics_color = {
-    color_error = { fg = colors.red },
-    color_warn = { fg = colors.yellow },
-    color_info = { fg = colors.cyan },
+    lualine_a = {
+      {
+        "mode",
+        function(str)
+          return str
+        end,
+        color = function()
+          return { bg = mode_color[vim.fn.mode()], fg = colors.background_dark }
+        end,
+        padding = { left = 1, right = 1 },
+      },
+    },
+    lualine_b = {
+      {
+        'branch',
+        icon = '',
+        color = { bg = colors.bg_light, fg = colors.comment },
+      },
+    },
+    lualine_c = {
+      {
+        'filename',
+        cond = conditions.buffer_not_empty,
+        color = { fg = colors.comment },
+      },
+      {
+        'diff',
+        symbols = { added = ' ', modified = ' ', removed = ' ' },
+        diff_color = {
+          added = { fg = colors.soft_green },
+          modified = { fg = colors.orange },
+          removed = { fg = colors.red },
+        },
+        cond = conditions.hide_in_width,
+      },
+      {
+        'diagnostics',
+        sources = { 'nvim_diagnostic' },
+        symbols = { error = ' ', warn = ' ', info = ' ' },
+        diagnostics_color = {
+          color_error = { bg = colors.red },
+          color_warn = { bg = colors.yellow },
+          color_info = { bg = colors.cyan },
+        },
+      },
+    },
+    lualine_x = {
+    },
+    lualine_y = {
+      {
+        'encoding',
+        color = { fg = colors.comment },
+      },
+      {
+        'fileformat',
+        color = { fg = colors.comment },
+      },
+      {
+        'filetype',
+        icons_enabled = true,
+        color = { fg = colors.comment },
+      },
+    },
+    lualine_z = {
+      {
+        'location',
+        color = function()
+          return { bg = mode_color[vim.fn.mode()], fg = colors.background_dark }
+        end,
+      },
+    },
   },
 }
 
-ins_right {
-  'location'
-}
-
-ins_right {
-  function()
-    return '▊'
-  end,
-  color = { fg = colors.blue },
-  padding = { left = 1 },
-}
-
--- Now don't forget to initialize lualine
 lualine.setup(config)
