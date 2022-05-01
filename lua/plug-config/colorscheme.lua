996b9512743bf0ece42cef3064bc812d60b6c9ba
Charlie <charlie@moren.dev>
2022-05-01 08:42:11 -0700

    update gruvbox config

--- lua/plug-config/colorscheme.lua
+++ lua/plug-config/colorscheme.lua
@@ -1,6 +1,6 @@
 -- Gruvbox --
 vim.g.gruvbox_baby_function_style = "NONE"
-vim.g.gruvbox_baby_keyword_style = "italic"
+vim.g.gruvbox_baby_keyword_style = "NONE"
 vim.g.gruvbox_baby_telescope_theme = 1
 vim.g.gruvbox_baby_background_color = "dark"
 vim.g.gruvbox_baby_comment_style = "italic"
