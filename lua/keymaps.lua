3659368b27321d225e0fdea4d4888193579e0f7f
Charlie Moren <charlie.moren@softwarfare.com>
2022-05-23 11:19:47 -0500

    add refactoring keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -18,6 +18,20 @@ vim.g.maplocalleader = " "
 --   term_mode = "t",
 --   command_mode = "c",
 
+-- Refactoring --
+-- Remaps for the refactoring operations currently offered by the plugin
+keymap("v", "<leader>re", [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function')<cr>]], opts)
+keymap("v", "<leader>rf", [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function To File')<cr>]], opts)
+keymap("v", "<leader>rv", [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Variable')<cr>]], opts)
+keymap("v", "<leader>ri", [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<cr>]], opts)
+
+-- Extract block doesn't need visual mode
+keymap("n", "<leader>rb", [[ <Cmd>lua require('refactoring').refactor('Extract Block')<cr>]], opts)
+keymap("n", "<leader>rbf", [[ <Cmd>lua require('refactoring').refactor('Extract Block To File')<cr>]], opts)
+
+-- Inline variable can also pick up the identifier currently under the cursor without visual mode
+keymap("n", "<leader>ri", [[ <Cmd>lua require('refactoring').refactor('Inline Variable')<cr>]], opts)
+
 -- NvimTree --
 keymap("n", "<leader>nt", ":NvimTreeToggle<cr>", opts)
 keymap("n", "<leader>nf", ":NvimTreeFindFile<cr>", opts)
